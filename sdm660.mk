73f627ef4621b12bb637a2227f4f6168a5cf8850
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-08-26 02:48:37 +0200

    sdm660-common: rootdir: Initial libperfmgr init config
    * From coral, heavily stripped down
    
    Change-Id: Ib6846e4f4f23baf56910d2330e26846b99830218

--- sdm660.mk
+++ sdm660.mk
@@ -254,6 +254,10 @@ PRODUCT_PACKAGES += \
     libipanat \
     liboffloadhal
 
+# IRQ
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
+
 # IRSC
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
@@ -371,6 +375,7 @@ PRODUCT_PACKAGES += \
     init.class_main.sh \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
+    init.qcom.power.rc \
     init.qcom.sensors.sh \
     init.qcom.sh \
     init.qti.qseecomd.sh \
