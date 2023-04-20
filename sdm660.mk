be44d6a48caf80017d9613e0d5742ff83654c455
Erfan Abdi <erfangplus@gmail.com>
2023-04-20 22:58:18 +0330

    sdm660-common: Update bootctrl HAL to 1.2

--- sdm660.mk
+++ sdm660.mk
@@ -137,9 +137,9 @@ PRODUCT_PACKAGES += \
 
 # Boot Control
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-impl-qti \
-    android.hardware.boot@1.1-impl-qti.recovery \
-    android.hardware.boot@1.1-service \
+    android.hardware.boot@1.2-impl-qti \
+    android.hardware.boot@1.2-impl-qti.recovery \
+    android.hardware.boot@1.2-service \
     bootctrl.sdm660 \
     bootctrl.sdm660.recovery
 
