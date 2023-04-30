913b6b4d710fdcbf39aa8f0e98bb488b0dc1b80f
ARINDAM BHATTACHARJEE <abhattacharjee717@gmail.com>
2023-04-30 17:34:31 +0000

    sdm660-common: Update audio HAL & HIDL version to v7.x
    Co-authored-by: Arindam Bhattacharjee<abhattacharjee717@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -94,9 +94,9 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@6.0-impl \
+    android.hardware.audio@7.1-impl \
     android.hardware.audio.service \
-    android.hardware.audio.effect@6.0-impl \
+    android.hardware.audio.effect@7.0-impl \
     android.hardware.soundtrigger@2.2-impl \
     audio.a2dp.default \
     audio.primary.sdm660 \
