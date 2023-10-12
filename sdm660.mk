LineageOS/android_device_xiaomi_sdm660-common

sdm660-common: Remove QTI perfd
Bug: 69270928
Test: Build and boot device and test camera
Change-Id: I87b283206f462fb5c8ec9cdd303ae2934fe9bfc4

sdm660.mk
@@ -317,8 +317,7 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2.vendor \
-    android.hardware.power-service.xiaomi-libperfmgr \
-    vendor.qti.hardware.perf@2.2.vendor
+    android.hardware.power-service.xiaomi-libperfmgr
 
 PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \

Nokia-SDM660-Devices/android_device_nokia_sdm660-common

sdm660.mk
@@ -320,8 +320,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.nokia-libperfmgr \
-    vendor.qti.hardware.perf@2.0.vendor
+    android.hardware.power-service.nokia-libperfmgr
 
 # Protobuf
 PRODUCT_PACKAGES += \