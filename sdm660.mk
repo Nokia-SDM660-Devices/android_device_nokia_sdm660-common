864357060f1117ef371a3ef560b3b1ce53a66d50
userariii <abhattacharjee717@gmail.com>
2023-11-26 01:43:49 +0530

    sdm660-common: Implement Retrofit Dynamic Partitions [2/2]
    refs: https://source.android.com/docs/core/ota/dynamic_partitions/implement
    refs: https://github.com/log1cs/android_device_nokia_NLA/commit/92a1998ca00f7209592a66b9b59b7018eb46ea29
    
    Signed-off-by: userariii <abhattacharjee717@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -191,6 +191,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libcutils_shim
 
+# Dynamic Partitions
+PRODUCT_BUILD_SUPER_PARTITION := false
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_RETROFIT_DYNAMIC_PARTITIONS := true
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service \
